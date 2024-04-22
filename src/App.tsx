3b75da4ac00aa9f7db0e1bca03cd6f06f8916848
Miri Hoogi <mirihoogi@gmail.com>
2024-04-22 14:45:40 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
 import { useSignal } from '@preact/signals-react';
+import { Document, Page } from 'react-pdf';
+import { Box } from '@mui/material';
 import './App.css'
 
 function App() {
-  const isOpen = useSignal(true);
-  const toggle = () => {
-    // no changes
-    isOpen.value = !isOpen.value;
-  };
-
-  //not printing
-  console.log(isOpen.value)
 
   return (
     <>
-       <div style={{ color: 'red', width: '100%', height: '100%',border:"1px solid red" }} onClick={toggle} >
-          {isOpen.value ? 'signal true' : 'signal false'}
-        </div>
+       <Box style={{ height: '300px', overflow: 'auto' }}>
+       <Document file='/production.pdf'>
+        <Page pageNumber={1} />
+      </Document>
+    </Box>
     </>
   )
 }
